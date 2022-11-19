6ead213226e2cf4c48a749386caa55885b21d257
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2022-11-19 17:16:32 -0500

    Refactor frontend into root folder
    Refactor frontend into root folder
    Rebuild using yarn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Homepage from "./components/Homepage";
+import "antd/dist/antd.css";
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Homepage />}>
+          
+        </Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+      
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
