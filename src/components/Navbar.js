bde7927856fedc3926edd37a12a0479d1a9d1f12
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2022-11-25 16:54:29 -0500

    Refactor User Checks
    Refactor user checks to determine if user is signed in
    Refactor navigation to reflect proper auth state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,20 @@
 import React , { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import jwt_decode from "jwt-decode";
-import Login from './Login';
-import Register from './Register';
 import { Row, Col, Button } from 'antd';
-import { Amplify } from 'aws-amplify';
+import { useNavigate } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from '../aws-exports';
  
 const Navbar = ({ signOut, user }) => {
-    const [name, setName] = useState('');
-    const [token, setToken] = useState('');
-    const [expire, setExpire] = useState('');
+
     const navigate = useNavigate();
- 
-    useEffect(() => {
-        refreshToken();
-    }, []);
- 
-    const refreshToken = async () => {
-        try {
-            const response = await axios.get('http://localhost:4000/token');
-            setToken(response.data.accessToken);
-            const decoded = jwt_decode(response.data.accessToken);
-            setName(decoded.name);
-            setExpire(decoded.exp);
-        } catch (error) {
-            if (error.response) {
-                navigate("/");
-            }
-        }
-    }
- 
-    const axiosJWT = axios.create();
+
+    const handleSignOut = () => {
+        goToHomePage()
+        signOut()
+    };
+
+    const goToHomePage = () => {
+        navigate("/");
+    };
 
     if(user) { //If logged in
         return (
@@ -45,7 +25,7 @@ const Navbar = ({ signOut, user }) => {
                     <Col span={2}>
                         {user.username}
                     </Col>
-                    <Col span={1}><Button  onClick={signOut} >Logout</Button></Col>
+                    <Col span={1}><Button  onClick={handleSignOut} >Logout</Button></Col>
                 </Row>
           </>
         );
@@ -58,7 +38,6 @@ const Navbar = ({ signOut, user }) => {
                 <Col span={12}></Col>
                 <Col span={4}></Col>
                 <Col span={4}>
-                <button onClick={signOut}>Sign out</button>
                 </Col>
             </Row>
         </>
