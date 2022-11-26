56a65a07edf72fea8880c107df54dedd371d1687
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2022-11-25 22:07:11 -0500

    List uploaded PDF files
    List uploaded pdfs in dashboard
    Refactor uploading (note it may take a second to finish uploading after you close the window, but it will update automatically)
    Provide utility to get key of any PDF in list (pdflist[n].key)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import jwt_decode from "jwt-decode";
-import { useNavigate } from 'react-router-dom';
 import Navbar from "./Navbar";
-import Upload from "./Upload"
-import { Auth, Amplify } from 'aws-amplify';
+import { Auth, Amplify, Storage } from 'aws-amplify';
 import { Breadcrumb, Layout, Button, Modal, Space, Divider, Row, Col, Table, Tag } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
+Storage.configure({ level: 'protected' });
 
- 
 const Dashboard = ({user}) => {
     const [name, setName] = useState('');
     const [token, setToken] = useState('');
@@ -19,9 +15,7 @@ const Dashboard = ({user}) => {
     const [numPdf, setNumPdf] = useState(4);
     const [selectedFile, setSelectedFile] = useState(null);
     const [loaded, setLoaded] = useState(0);
-
-    
-
+    const [pdflist, setPdfList] = useState(new Array());
     const displayColumns = [
         {
           title: 'Name',
@@ -30,86 +24,90 @@ const Dashboard = ({user}) => {
           render: (text) => <a>{text}</a>,
         },
         {
-          title: 'Length',
-          dataIndex: 'length',
-          key: 'length',
+          title: 'Size',
+          dataIndex: 'size',
+          key: 'size',
         },
         {
           title: 'Last Edited:',
           dataIndex: 'lastedit',
           key: 'lastedit',
-        },
-        {
-          title: 'Tags',
-          key: 'tags',
-          dataIndex: 'tags',
-          render: (_, { tags }) => (
-            <Space size = "middle">
-              {tags.map((tag) => {
-                let color;
-                //TODO: Generalize with user tags
-                switch (tag) {
-                    case 'tag1': color = 'green' 
-                        break;
-                    case 'tag2': color = 'cadetblue'
-                        break;
-                    case 'tag3': color = 'crimson'
-                        break;
-                    case 'tag4': color = 'blue'
-                        break;
-                    case 'math': color = 'blue'
-                        break;
-                    case 'urgent': color = 'red'
-                        break;
-                    default: color = 'black'
-                }
-                return (
-                  <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                );
-              })}
-            </Space>
-          ),
         }
     ];
-      
-    const pdfListData = new Array(numPdf);
-    
-    //TODO: Get document specific data from index
-    
-    for (let index = 0; index < pdfListData.length; index++) {
-        pdfListData[index] = {
-        key: index.toString,
-        name: 'Document ' + index,
-        length: 42,
-        lastedit:'Just Now',
-        tags: ['tag1', 'tag2', 'tag4']
-        }
+
+    const handleCancel = () => {
+        setSelectedFile(null);
+        setIsModalOpen(false);
+        getList();
+    };
+
+    async function handleUpload(e) {
+        const file = e.target.files[0];
+        let key = file.name
+        console.log("key: " + key)
+        try {
+            await Storage.put(file.name, file, {
+              contentType: "application/pdf", // contentType is optional
+            });
+          } catch (error) {
+            console.log("Error uploading file: ", error);
+          }
+        handleCancel()
     }
 
-    async function getUserName () { 
-        let user = await Auth.currentAuthenticatedUser();
-        const { attributes } = user;
-        return attributes;
-    }  
- 
+    useEffect(() => {
+        Storage.list('', { level: 'protected' })
+            .then(({ results }) => {
+            console.log(results)
+            const pdfListData = new Array(results.legnth)
+            for (let index = 0; index < results.length; index++) {
+                pdfListData[index] = {
+                key: index.toString,
+                name: results[index].key,
+                size: results[index].size + ' B',
+                lastedit: results[index].lastModified.toISOString()
+            }
+            setPdfList(pdfListData)
+        }
+        });
+        console.log(pdflist)
+    }, []);
+     
+    function getList() {
+        Storage.list('', { level: 'protected' })
+            .then(({ results }) => {
+            console.log(results)
+            const pdfListData = new Array(results.legnth)
+            for (let index = 0; index < results.length; index++) {
+                pdfListData[index] = {
+                key: index.toString,
+                name: results[index].key,
+                size: results[index].size + ' B',
+                lastedit: results[index].lastModified.toISOString()
+            }
+            setPdfList(pdfListData)
+        }
+        });
+        console.log(pdflist)
+    }
     return (
         <>
         <Layout className="layout">
             <Header style={{ backgroundColor: '#b6d7a8' }}>
                 <Navbar/>
-                <Col flex={0}><Upload/></Col>
-            </Header>
+                <Col flex={0}>
+                    <input type="file" accept=".pdf" className="input" placeholder="Username" value={selectedFile} onChange={handleUpload} />
+                </Col>
+            </Header>       
             <Content style={{ padding: '0 50px' }}>
                 <Layout>
                 <Divider />
                 <div className="site-layout-content">
-                    <Table columns={displayColumns} dataSource={pdfListData} />
+                    <Table columns={displayColumns} dataSource={pdflist} />
                 </div>
                 </Layout>
             </Content>
-            <Footer style={{ textAlign: 'center' }}>Luis Sergovia Fan Club ©2022 Created by James Redding & Maxwell Ryan</Footer>
+            <Footer style={{ textAlign: 'center' }}>Luis Segovia Fan Club ©2022 Created by James Redding & Maxwell Ryan</Footer>
         </Layout>
         </>
     )
